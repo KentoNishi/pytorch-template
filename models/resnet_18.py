d3276c14f9ddb634cd4e7276df9ac906e00b3373
Kento Nishi <kento24gs@outlook.com>
2022-07-22 14:16:44 -0700

    weights=None

--- models/resnet_18.py
+++ models/resnet_18.py
@@ -3,6 +3,6 @@ import torchvision.models as models
 
 
 def ResNet_18():
-    model = models.resnet18(pretrained=False)
+    model = models.resnet18(weights=None)
     model.fc = torch.nn.Linear(512, 10)
     return model
